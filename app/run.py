63ac841935422df2e86af5ade88e91dd3f29a370
Charles Joseph <chrlsjsph460@gmail.com>
2022-07-25 21:32:40 -0500

    Add files via upload
    Add graphics to run.py. show bargraph of positive cases and box plot of word counts.

--- app/run.py
+++ app/run.py
@@ -1,15 +1,20 @@
 import json
 import plotly
 import pandas as pd
+import numpy as np
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+from plotly.graph_objs import Bar, Histogram, Box
+import joblib
 from sqlalchemy import create_engine
+import sys
+
+# adding models folder to the system path
+sys.path.insert(0, r"C:\Users\charl\Desktop\Udacity_DataScience\Disaster-Response-Pipeline-Project\models")
 
 
 app = Flask(__name__)
@@ -26,12 +31,14 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+db_filepath = r"..\data\clean_messages.db"
+engine = create_engine(f'sqlite:///{db_filepath}')
+df = pd.read_sql_table('Messages_Clean', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
-
+model = joblib.load(r"..\models\saved_model.pkl")
+messages = df.message.apply(tokenize)
+message_wordcounts = df.message.apply(len)
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
@@ -42,7 +49,13 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+
+    categories = df.iloc[:,4:].sum(axis=0).sort_values(ascending = False)
+    category_names = categories.index.tolist()
+    category_counts = categories.values.tolist()
     
+    print(genre_counts)
+    print(genre_names)
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -63,9 +76,54 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Labels',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Label"
+                }
+            }
+        },
+        {
+            'data': [
+                Box(
+                    y=np.log10(message_wordcounts),
+                    marker = dict(color = "#c6e2ff"),
+                    boxpoints = "outliers",
+                    name = "",
+                    jitter = 0.3
+                    
+                )
+            ],
+
+            'layout': {
+                'title': 'Words Per Message',
+                'yaxis': {
+                    'title': "log(Count)"
+                },
+                'xaxis': {
+                    'title': ""
+                }
+            }
         }
     ]
-    
+    # Extract verbs and nouns for some underrepresented categories
+    # make pie chart
+    # earthquake, water, medical_pro, and death
+ 
+    print(genre_names)
+    print(genre_counts)
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
@@ -98,3 +156,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
