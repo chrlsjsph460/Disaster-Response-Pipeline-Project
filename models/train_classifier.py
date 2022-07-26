180964ee9c58f06e9e9e217568d14a32a9aefb2c
Charles Joseph <chrlsjsph460@gmail.com>
2022-07-25 21:31:14 -0500

    Add files via upload
    Add train_classifier.py and saved_model.pkl (The model is a pipeline: countvectorizer -> tfidf -> multinomialnb. For loop to choose countverctorizer params and gridsearchcv to choose tfidf and multinomialnb)

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,77 +1,29 @@
 import sys
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-import re
-import pandas as pd
-import numpy as np
-import sqlalchemy as db
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.multioutput import MultiOutputClassifier
-from sklearn.ensemble import RandomForestClassifier
+import time
 from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-
-def load_data(database_filepath):
-    engine = db.create_engine(database_filepath)
-    df = pd.read_sql_table("Messages_Clean", engine)
-    X = df.messages
-    Y = df.iloc[:,4:]
-    category_names = Y.columns
-    return X, Y, category_names
-
-
-def tokenize(text):
-    #change to lower case and remove non alpha numeric characters
-    text = re.sub(r"[^a-z0-9]", " ", text.lower())
-    #split message into words
-    words = word_tokenize(text)
-    #remove stop words
-    words = [word for word in words if word not in set(stopwords.words("english"))]
-    #use stemmer
-    stemmer = SnowballStemmer(language="english")
-    words = [stemmer.stem(word) for word in words]
-    return words
-
-
-def build_model():
-    pipeline = Pipeline([('count_vect',CountVectorizer(tokenizer=tokenize)), 
-                    ('tfidf',TfidfTransformer()),
-                    ('clf', MultiOutputClassifier(RandomForestClassifier()))])
-    return pipeline
-
-
-def evaluate_model(model, X_test, Y_test, category_names):
-    col_names = Y_test.columns 
-    y_pred = model.predict(X_test)
-    i = 0
-    y_pred_df = pd.DataFrame()
-    for name in col_names:
-        y_pred_df[name] = y_pred[:,i]
-        i += 1
-        report = classification_report(Y_test[name],y_pred_df[name],output_dict = True)
-        print(report)
-
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer, TfidfVectorizer
+import pandas as pd
+from helper_functions import load_data, tokenize, build_model, evaluate_model, save_model, train_model, CustomMultiOutputClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+      
 
 def main():
+    for arg in sys.argv:
+        print(arg)
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X, Y = load_data(database_filepath)
         
         print('Building model...')
         model = build_model()
         
         print('Training model...')
-        model.fit(X_train, Y_train)
+        start = time.time()
+        model = train_model(X, Y, model)
+        end = time.time()
+        print(f"Training took {(end-start)/3600} hours....")
         
-        print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
