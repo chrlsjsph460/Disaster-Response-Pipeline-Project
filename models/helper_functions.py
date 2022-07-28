db06b83a54aedd513a8730e86ac8c8e597b8c67e
Charles Joseph <chrlsjsph460@gmail.com>
2022-07-28 11:25:41 -0500

    Add files via upload
    Add starting verb extractor class. This class determines if a sentence has a verb as first word.

--- models/helper_functions.py
+++ models/helper_functions.py
@@ -42,6 +42,26 @@ import time
 
 hamming_scorer = make_scorer(hamming_loss, greater_is_better = False)
 
+#More insightful feature. Is the first tag a verb???
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return 1
+        return 0
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+
 # Can be used without MultiOutputClassifier
 class CustomKnnClassifier(BaseEstimator, ClassifierMixin):
     ''' assume pandas df for training.
